3e1b7543121b6b5e7a3390fb21731eea2e1bc4ae
colharris113 <87669967+colharris113@users.noreply.github.com>
2025-06-19 20:53:29 +0100

    Update for non important files

--- dist/routes/index.d.ts
+++
@@ -1,5 +0,0 @@
-export { default as customerRoutes } from './customerRoutes';
-export { default as categoryRoutes } from './categoryRoutes';
-export { default as shopItemRoutes } from './shopItemRoutes';
-export { default as orderRoutes } from './orderRoutes';
-//# sourceMappingURL=index.d.ts.map
--- dist/services/OrderService.d.ts
+++
@@ -1,9 +0,0 @@
-import { Order, CreateOrderRequest, UpdateOrderRequest } from '../models';
-export declare class OrderService {
-    getAllOrders(): Order[];
-    getOrderById(id: number): Order | null;
-    createOrder(orderData: CreateOrderRequest): Order;
-    updateOrder(id: number, updates: UpdateOrderRequest): Order | null;
-    deleteOrder(id: number): boolean;
-}
-//# sourceMappingURL=OrderService.d.ts.map
